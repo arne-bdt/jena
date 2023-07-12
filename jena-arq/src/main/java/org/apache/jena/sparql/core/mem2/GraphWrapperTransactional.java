7c6869d947668e215a8d24e2a9de0ea77eb968b8
Arne Bernhardt <arne.bernhardt@soptim.de>
2023-07-12 19:31:38 +0200

    DatasetGraphMem2Fast supporting MRPlusSW. - experimental - some things work now with GraphWrapperTransactional - next:   2. implement more unit tests   3. implement DatasetGraphMem2Fast   4. find out how to implement getTransactionHandler

--- jena-arq/src/main/java/org/apache/jena/sparql/core/mem2/GraphWrapperTransactional.java
+++ jena-arq/src/main/java/org/apache/jena/sparql/core/mem2/GraphWrapperTransactional.java
@@ -25,6 +25,7 @@ import org.apache.jena.query.TxnType;
 import org.apache.jena.shared.AddDeniedException;
 import org.apache.jena.shared.DeleteDeniedException;
 import org.apache.jena.shared.PrefixMapping;
+import org.apache.jena.sparql.JenaTransactionException;
 import org.apache.jena.sparql.core.Transactional;
 import org.apache.jena.util.iterator.ExtendedIterator;
 import org.slf4j.Logger;
@@ -33,7 +34,6 @@ import org.slf4j.LoggerFactory;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Supplier;
@@ -47,13 +47,13 @@ public class GraphWrapperTransactional implements Graph, Transactional {
 
     private final TransactionCoordinator transactionCoordinator;
     private final ConcurrentLinkedQueue<FastDeltaGraph> deltasToApplyToStaleGraph = new ConcurrentLinkedQueue<>();
-    private final Semaphore semaphoreForUpdatingStaleGraph = new Semaphore(1);
+    private final ReentrantLock lockForUpdatingStaleGraph = new java.util.concurrent.locks.ReentrantLock();
 
     private final ReentrantLock lockForBeginTransaction = new java.util.concurrent.locks.ReentrantLock();
     private final ForkJoinPool forkJoinPool;
     private volatile GraphWrapperChainingDeltasTransactional staleGraph;
     private volatile GraphWrapperChainingDeltasTransactional activeGraph;
-
+    private final ThreadLocal<GraphWrapperChainingDeltasTransactional> txnGraph = new ThreadLocal<>();
     private final AtomicBoolean delayedUpdateHasBeenScheduled = new AtomicBoolean(false);
 
     public GraphWrapperTransactional(final TransactionCoordinator transactionCoordinator) {
@@ -94,10 +94,24 @@ public class GraphWrapperTransactional implements Graph, Transactional {
         return transactionCoordinator;
     }
 
+    private GraphWrapperChainingDeltasTransactional getGraphForCurrentTransaction() {
+        final var txnGraph = this.txnGraph.get();
+        if (txnGraph == null) {
+            throw new JenaTransactionException("Not in a transaction.");
+        }
+        return txnGraph;
+    }
+
     @Override
     public void begin(TxnType txnType) {
-        switchStaleAndActiveIfNeededAndPossible();
-        activeGraph.begin(txnType);
+        try {
+            lockForBeginTransaction.lock();
+            switchStaleAndActiveIfNeededAndPossible();
+            txnGraph.set(activeGraph);
+            activeGraph.begin(txnType);
+        } finally {
+            lockForBeginTransaction.unlock();
+        }
     }
 
     private void switchStaleAndActiveIfNeededAndPossible() {
@@ -105,7 +119,7 @@ public class GraphWrapperTransactional implements Graph, Transactional {
                 && !staleGraph.hasOpenReadTransactions()     // only if there are no open read transactions on the stale graph
                 && !activeGraph.hasOpenWriteTransaction()) { // and there is no active write transaction
             try {
-                semaphoreForUpdatingStaleGraph.acquire();            // wait for the stale graph to be updated
+                lockForUpdatingStaleGraph.lock();            // wait for the stale graph to be updated
                 // check again, because it could have changed in the meantime
                 if (this.deltasToApplyToStaleGraph.isEmpty() //check if all deltas have been applied to the stale graph
                         && activeGraph.getLengthOfDeltaChain() > 0
@@ -115,52 +129,54 @@ public class GraphWrapperTransactional implements Graph, Transactional {
                     staleGraph = activeGraph;
                     activeGraph = tmp;
                 }
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
             } finally {
-                semaphoreForUpdatingStaleGraph.release();
+                lockForUpdatingStaleGraph.unlock();
             }
         }
     }
 
     @Override
     public ReadWrite transactionMode() {
-        return activeGraph.transactionMode();
+        return getGraphForCurrentTransaction().transactionMode();
     }
 
     @Override
     public TxnType transactionType() {
-        return activeGraph.transactionType();
+        return getGraphForCurrentTransaction().transactionType();
     }
 
     @Override
     public boolean isInTransaction() {
-        return activeGraph.isInTransaction();
+        return getGraphForCurrentTransaction().isInTransaction();
     }
 
     @Override
     public boolean promote(Promote txnType) {
-        return activeGraph.promote(txnType);
+        return getGraphForCurrentTransaction().promote(txnType);
     }
 
     @Override
     public void commit() {
-        activeGraph.commit();
-        if (!this.deltasToApplyToStaleGraph.isEmpty()) {
-            if (activeGraph.getLengthOfDeltaChain() > MAX_DELTA_CHAIN_LENGTH) {
-                // if there are many consecutive write transactions, they might always be faster than the async call
-                // to update the stale graph. That way, they might block the update for a long time.
-                // In this case, we call the update synchronously to avoid this problem.
-                this.updateStaleGraphIfPossible();
+        try {
+            getGraphForCurrentTransaction().commit();
+            if (!this.deltasToApplyToStaleGraph.isEmpty()) {
+                if (getGraphForCurrentTransaction().getLengthOfDeltaChain() > MAX_DELTA_CHAIN_LENGTH) {
+                    // if there are many consecutive write transactions, they might always be faster than the async call
+                    // to update the stale graph. That way, they might block the update for a long time.
+                    // In this case, we call the update synchronously to avoid this problem.
+                    this.updateStaleGraphIfPossible();
+                }
+                forkJoinPool.execute(this::updateStaleGraphIfPossible);
             }
-            forkJoinPool.execute(this::updateStaleGraphIfPossible);
+        } finally {
+            txnGraph.remove();
         }
     }
 
     private void updateStaleGraphIfPossible() {
         if (!staleGraph.hasOpenReadTransactions()) {
+            lockForUpdatingStaleGraph.lock();
             try {
-                semaphoreForUpdatingStaleGraph.acquire();
                 while (!deltasToApplyToStaleGraph.isEmpty()) {
                     final var delta = deltasToApplyToStaleGraph.peek();
                     staleGraph.mergeDeltasAndExecuteDirectlyOnWrappedGraph(stale -> {
@@ -172,18 +188,19 @@ public class GraphWrapperTransactional implements Graph, Transactional {
             } catch (Throwable throwable) {
                 LOGGER.error("Error while updating stale graph.", throwable);
             } finally {
-                semaphoreForUpdatingStaleGraph.release();
+                lockForUpdatingStaleGraph.unlock();
             }
-        } else if (!delayedUpdateHasBeenScheduled.get()) { // avoid scheduling multiple delayed updates
+        } else {
             // While there are still open read transactions, we wait a bit and try again.
             // There are no new read transactions possible, so we will eventually succeed.
             // The only caller of this method is the commit method. This is the only place
             // where we add deltas to the queue and that here is work to do.
-            delayedUpdateHasBeenScheduled.set(true);
-            CompletableFuture.delayedExecutor(
-                            transactionCoordinator.getStaleTransactionRemovalTimerIntervalMs(),
-                            java.util.concurrent.TimeUnit.MILLISECONDS, forkJoinPool)
-                    .execute(this::delayedUpdateStaleGraphIfPossible);
+            if (delayedUpdateHasBeenScheduled.compareAndSet(false, true)) {
+                CompletableFuture.delayedExecutor(
+                                transactionCoordinator.getStaleTransactionRemovalTimerIntervalMs(),
+                                java.util.concurrent.TimeUnit.MILLISECONDS, forkJoinPool)
+                        .execute(this::delayedUpdateStaleGraphIfPossible);
+            }
         }
     }
 
@@ -194,107 +211,115 @@ public class GraphWrapperTransactional implements Graph, Transactional {
 
     @Override
     public void abort() {
-        activeGraph.abort();
+        try {
+            getGraphForCurrentTransaction().abort();
+        } finally {
+            txnGraph.remove();
+        }
     }
 
     @Override
     public void end() {
-        activeGraph.end();
+        try {
+            getGraphForCurrentTransaction().end();
+        } finally {
+            txnGraph.remove();
+        }
     }
 
     @Override
     public boolean dependsOn(Graph other) {
-        return activeGraph.dependsOn(other);
+        return getGraphForCurrentTransaction().dependsOn(other);
     }
 
     @Override
     public TransactionHandler getTransactionHandler() {
-        return null; // TODO
+        return getGraphForCurrentTransaction().getTransactionHandler();
     }
 
     @Override
     public Capabilities getCapabilities() {
-        return activeGraph.getCapabilities();
+        return getGraphForCurrentTransaction().getCapabilities();
     }
 
     @Override
     public GraphEventManager getEventManager() {
-        return activeGraph.getEventManager();
+        return getGraphForCurrentTransaction().getEventManager();
     }
 
     @Override
     public PrefixMapping getPrefixMapping() {
-        return activeGraph.getPrefixMapping();
+        return getGraphForCurrentTransaction().getPrefixMapping();
     }
 
     @Override
     public void add(Triple t) throws AddDeniedException {
-        activeGraph.add(t);
+        getGraphForCurrentTransaction().add(t);
     }
 
     @Override
     public void add(Node s, Node p, Node o) throws AddDeniedException {
-        activeGraph.add(s, p, o);
+        getGraphForCurrentTransaction().add(s, p, o);
     }
 
     @Override
     public void delete(Triple t) throws DeleteDeniedException {
-        activeGraph.delete(t);
+        getGraphForCurrentTransaction().delete(t);
     }
 
     @Override
     public void delete(Node s, Node p, Node o) throws DeleteDeniedException {
-        activeGraph.delete(s, p, o);
+        getGraphForCurrentTransaction().delete(s, p, o);
     }
 
     @Override
     public ExtendedIterator<Triple> find(Triple m) {
-        return activeGraph.find(m);
+        return getGraphForCurrentTransaction().find(m);
     }
 
     @Override
     public ExtendedIterator<Triple> find(Node s, Node p, Node o) {
-        return activeGraph.find(s, p, o);
+        return getGraphForCurrentTransaction().find(s, p, o);
     }
 
     @Override
     public Stream<Triple> stream(Node s, Node p, Node o) {
-        return activeGraph.stream(s, p, o);
+        return getGraphForCurrentTransaction().stream(s, p, o);
     }
 
     @Override
     public Stream<Triple> stream() {
-        return activeGraph.stream();
+        return getGraphForCurrentTransaction().stream();
     }
 
     @Override
     public ExtendedIterator<Triple> find() {
-        return activeGraph.find();
+        return getGraphForCurrentTransaction().find();
     }
 
     @Override
     public boolean isIsomorphicWith(Graph g) {
-        return activeGraph.isIsomorphicWith(g);
+        return getGraphForCurrentTransaction().isIsomorphicWith(g);
     }
 
     @Override
     public boolean contains(Node s, Node p, Node o) {
-        return activeGraph.contains(s, p, o);
+        return getGraphForCurrentTransaction().contains(s, p, o);
     }
 
     @Override
     public boolean contains(Triple t) {
-        return activeGraph.contains(t);
+        return getGraphForCurrentTransaction().contains(t);
     }
 
     @Override
     public void clear() {
-        activeGraph.clear();
+        getGraphForCurrentTransaction().clear();
     }
 
     @Override
     public void remove(Node s, Node p, Node o) {
-        activeGraph.remove(s, p, o);
+        getGraphForCurrentTransaction().remove(s, p, o);
     }
 
     @Override
@@ -305,12 +330,12 @@ public class GraphWrapperTransactional implements Graph, Transactional {
 
     @Override
     public boolean isEmpty() {
-        return activeGraph.isEmpty();
+        return getGraphForCurrentTransaction().isEmpty();
     }
 
     @Override
     public int size() {
-        return activeGraph.size();
+        return getGraphForCurrentTransaction().size();
     }
 
     @Override
