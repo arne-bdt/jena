aa025ab721025c3d514c3d748f5ff8e1f45018d1
Arne Bernhardt <arne.bernhardt@soptim.de>
2023-09-28 00:31:47 +0200

    DatasetGraphMem2Fast supporting MRPlusSW. State: still experimental - but it works so far Latest changes: - refactored TestMEAS to JMH test
    - next:
      2. implement more unit tests for GraphWrapperTransactional
      3. implement DatasetGraphMem2Fast
      4. find out how to implement getTransactionHandler

--- jena-benchmarks/jena-benchmarks-jmh/src/test/java/org/apache/jena/mem/graph/TestMEAS.java
+++ jena-benchmarks/jena-benchmarks-jmh/src/test/java/org/apache/jena/mem/graph/TestMEAS.java
@@ -1,76 +1,90 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.jena.mem.graph;
 
-import org.apache.commons.lang3.time.StopWatch;
-import org.apache.jena.datatypes.xsd.XSDDatatype;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.jena.datatypes.xsd.XSDDateTime;
 import org.apache.jena.graph.Graph;
-import org.apache.jena.graph.GraphMemFactory;
 import org.apache.jena.graph.Node;
+import org.apache.jena.mem.GraphMem;
+import org.apache.jena.mem.graph.helper.JMHDefaultOptions;
+import org.apache.jena.mem2.GraphMem2Fast;
+import org.apache.jena.mem2.GraphMem2Legacy;
+import org.apache.jena.mem2.GraphMem2Roaring;
+import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.query.QuerySolution;
-import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Property;
+import org.apache.jena.sparql.core.DatasetGraph;
 import org.apache.jena.sparql.core.DatasetGraphFactory;
-import org.apache.jena.vocabulary.RDF;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
 
-import java.time.Clock;
 import java.time.Instant;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import java.util.UUID;
 
+@State(Scope.Benchmark)
 public class TestMEAS {
 
-    private static final Model m = ModelFactory.createDefaultModel();
+    @Param({
+            "GraphMem",
+            "GraphMem2Fast",
+            "GraphMem2Legacy",
+            "GraphMem2Roaring"
+    })
+    public String param0_GraphImplementation;
+    @Param({"90000"})
+    public String param1_numberOfAnalogValues;
+    @Param({"10000"})
+    public String param2_numberOfDiscreteValues;
+    private Graph sutGraph;
+    private DatasetGraph sutDatasetGraph;
+    private Query sutAnalogQuery;
+    private Query sutDiscreteQuery;
+    private List<QuerySolution> sutQuerySolutions;
+    private int numberOfAnalogValues;
+    private int numberOfDiscreteValues;
 
-    private static final String MEAS_NS = "http://www.fancyTSO.org/OurCIMModel/MEASv1#";
-
-    private static final String[] AnalogTypes = {"SomeAnalog", "ActivePowerAnalog", "ReactivePowerAnalog", "VoltageAnalog", "PhaseAngleAnalog", "GlobalRadiationAnalog", "HumidityAnalog", "TemperatureAnalog", "WindSpeedAnalog", "WindDirectionAnalog", "FrequencyAnalog", "PowerFactorAnalog", "CurrentAnalog"};
-    private static final String[] DiscreteTypes = {"PhaseChangerStep", "BreakerStatus", "SwitchStatus", "TapChangerStatus", "TapChangerStep", "TapChangerControlMode", "TapChangerNeutralStatus", "TapChangerKind", "TapChangerMode", "TapChangerControlKind"};
-
-    private static final Property AnalogValueValue = m.createProperty(MEAS_NS + "AnalogValue.value");
-    private static final Property DiscreteValueValue = m.createProperty(MEAS_NS + "DiscreteValue.value");
-
-    private static final Property MeasurementValueTimeStamp = m.createProperty(MEAS_NS + "MeasurementValue.timeStamp");
-    private static final Property MeasurementValueStatus = m.createProperty(MEAS_NS + "MeasurementValue.status");
-
-    public static Graph createGraphWithMEASData(int numberOfAnalogValues, int numberOfDiscreteValues) {
-        final var graph = GraphMemFactory.createGraphMem2Basic();
-        final var model = ModelFactory.createModelForGraph(graph);
-
-        final var random = new Random(4711);
-
-        final var analogTypeProvider = new NextStringInArrayProvider(AnalogTypes);
-        for (int i = 0; i < numberOfAnalogValues; i++) {
-            model.createResource("_" + UUID.randomUUID())
-                    .addProperty(RDF.type, model.createResource(MEAS_NS + analogTypeProvider.next()))
-                    .addProperty(MeasurementValueTimeStamp, DateTimeFormatter.ISO_INSTANT.format(Clock.systemUTC().instant()), XSDDatatype.XSDdateTimeStamp)
-                    .addProperty(MeasurementValueStatus, Integer.toString(random.nextInt()), XSDDatatype.XSDinteger)
-                    .addProperty(AnalogValueValue, Float.toString(random.nextFloat()), XSDDatatype.XSDfloat);
+    public Graph createGraph() {
+        switch (param0_GraphImplementation) {
+            case "GraphMem":
+                return new GraphMem();
+            case "GraphMem2Fast":
+                return new GraphMem2Fast();
+            case "GraphMem2Legacy":
+                return new GraphMem2Legacy();
+            case "GraphMem2Roaring":
+                return new GraphMem2Roaring();
+            default:
+                throw new IllegalArgumentException("Unknown graph implementation: " + param0_GraphImplementation);
         }
-
-        final var discreteTypeProvider = new NextStringInArrayProvider(DiscreteTypes);
-        for (int i = 0; i < numberOfDiscreteValues; i++) {
-            model.createResource("_" + UUID.randomUUID())
-                    .addProperty(RDF.type, model.createResource(MEAS_NS + discreteTypeProvider.next()))
-                    .addProperty(MeasurementValueTimeStamp, DateTimeFormatter.ISO_INSTANT.format(Clock.systemUTC().instant()), XSDDatatype.XSDdateTimeStamp)
-                    .addProperty(MeasurementValueStatus, Integer.toString(random.nextInt()), XSDDatatype.XSDinteger)
-                    .addProperty(DiscreteValueValue, Integer.toString(random.nextInt()), XSDDatatype.XSDinteger);
-        }
-
-        return graph;
     }
+
 
     private List<QuerySolution> queryAnalogAndDigitalValues(Graph graph) {
         var model = ModelFactory.createModelForGraph(graph);
 
         var result = new ArrayList<QuerySolution>(100000);
-        var stopWatch = StopWatch.createStarted();
         QueryExecutionFactory.create("""
                         PREFIX meas: <http://www.fancyTSO.org/OurCIMModel/MEASv1#>
                         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
@@ -91,52 +105,25 @@ public class TestMEAS {
                         """, model)
                 .execSelect()
                 .forEachRemaining(result::add);
-        stopWatch.stop();
-        System.out.println("Query took " + stopWatch.getTime() + "ms and returned " + result.size() + " results.");
-
         return result;
     }
 
-    private List<QuerySolution> queryAnalogAndDigitalValuesWithPreparedQueries(Graph graph) {
-        var datasetGraph = DatasetGraphFactory.wrap(graph);
-
+    private List<QuerySolution> queryAnalogAndDigitalValuesWithPreparedQueries(DatasetGraph datasetGraph) {
 
         var result = new ArrayList<QuerySolution>(100000);
-        var analogQuery = QueryFactory.create("""
-                PREFIX meas: <http://www.fancyTSO.org/OurCIMModel/MEASv1#>
-                PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-                SELECT ?s ?analogValue ?timeStamp ?status  
-                WHERE { ?s meas:AnalogValue.value ?analogValue;
-                           meas:MeasurementValue.timeStamp ?timeStamp;
-                           meas:MeasurementValue.status ?status.}
-                """);
-        var discreteQuery = QueryFactory.create("""
-                PREFIX meas: <http://www.fancyTSO.org/OurCIMModel/MEASv1#>
-                PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-                SELECT ?s ?discreteValue ?timeStamp ?status  
-                WHERE { ?s meas:DiscreteValue.value ?discreteValue;
-                           meas:MeasurementValue.timeStamp ?timeStamp;
-                           meas:MeasurementValue.status ?status.}
-                """);
 
-        var stopWatch = StopWatch.createStarted();
-
-        var queryExecutionAnalog = QueryExecutionFactory.create(analogQuery, datasetGraph);
-        var queryExecutionDiscrete = QueryExecutionFactory.create(discreteQuery, datasetGraph);
+        var queryExecutionAnalog = QueryExecutionFactory.create(sutAnalogQuery, datasetGraph);
+        var queryExecutionDiscrete = QueryExecutionFactory.create(sutDiscreteQuery, datasetGraph);
 
         queryExecutionAnalog.execSelect().forEachRemaining(result::add);
         queryExecutionDiscrete.execSelect().forEachRemaining(result::add);
 
-        stopWatch.stop();
-        System.out.println("Query took " + stopWatch.getTime() + "ms and returned " + result.size() + " results.");
-
         return result;
     }
 
-    private void fillQueryResultsIntoLists(List<QuerySolution> result) {
-        final var analogValues = new ArrayList<AnalogValue>(90000);
-        final var discreteValues = new ArrayList<DiscreteValue>(10000);
-        var stopWatch = StopWatch.createStarted();
+    private Pair<List<AnalogValue>, List<DiscreteValue>> fillQueryResultsIntoLists(List<QuerySolution> result) {
+        final var analogValues = new ArrayList<AnalogValue>(numberOfAnalogValues);
+        final var discreteValues = new ArrayList<DiscreteValue>(numberOfDiscreteValues);
         for (var querySolution : result) {
             if (querySolution.contains("analogValue")) {
                 analogValues.add(new AnalogValue((float) querySolution.getLiteral("analogValue").getValue(), ((XSDDateTime) querySolution.getLiteral("timeStamp").getValue()).asCalendar().toInstant(), (int) querySolution.getLiteral("status").getValue()));
@@ -144,91 +131,117 @@ public class TestMEAS {
                 discreteValues.add(new DiscreteValue((int) querySolution.getLiteral("discreteValue").getValue(), ((XSDDateTime) querySolution.getLiteral("timeStamp").getValue()).asCalendar().toInstant(), (int) querySolution.getLiteral("status").getValue()));
             }
         }
-        stopWatch.stop();
-        System.out.println("Iterating over " + result.size() + " results took " + stopWatch.getTime() + "ms.");
+        return Pair.of(analogValues, discreteValues);
     }
 
     @Test
     public void loadRDFSAndProfile() {
         /*TODO: Create JMH-Benchmark out of this to prevent the JVM from taking shortcuts */
+        final var g = new GraphMem2Fast();
 
-        var g = createGraphWithMEASData(90000, 10000);
+        MEASData.fillGraphWithMEASData(g, numberOfAnalogValues, numberOfDiscreteValues);
 
         var result = queryAnalogAndDigitalValues(g);
-        result = queryAnalogAndDigitalValues(g);
-        result = queryAnalogAndDigitalValues(g);
-        result = queryAnalogAndDigitalValues(g);
-        result = queryAnalogAndDigitalValues(g);
 
         fillQueryResultsIntoLists(result);
-        fillQueryResultsIntoLists(result);
-        fillQueryResultsIntoLists(result);
-        fillQueryResultsIntoLists(result);
-        fillQueryResultsIntoLists(result);
 
-        result = queryAnalogAndDigitalValuesWithPreparedQueries(g);
-        result = queryAnalogAndDigitalValuesWithPreparedQueries(g);
-        result = queryAnalogAndDigitalValuesWithPreparedQueries(g);
-        result = queryAnalogAndDigitalValuesWithPreparedQueries(g);
-        result = queryAnalogAndDigitalValuesWithPreparedQueries(g);
+        //result = queryAnalogAndDigitalValuesWithPreparedQueries(DatasetGraphFactory.wrap(g));
 
-        fillQueryResultsIntoLists(result);
-        fillQueryResultsIntoLists(result);
-        fillQueryResultsIntoLists(result);
-        fillQueryResultsIntoLists(result);
-        fillQueryResultsIntoLists(result);
+        //fillQueryResultsIntoLists(result);
 
-        fillListsByGraph(g);
-        fillListsByGraph(g);
-        fillListsByGraph(g);
-        fillListsByGraph(g);
         fillListsByGraph(g);
 
         int i = 0;
     }
 
-    private void fillListsByGraph(Graph g) {
-        final var analogValues = new ArrayList<AnalogValue>(90000);
-        final var discreteValues = new ArrayList<DiscreteValue>(10000);
-        var stopWatch = StopWatch.createStarted();
-        g.stream(Node.ANY, AnalogValueValue.asNode(), Node.ANY)
+//    @Benchmark
+//    public Graph createGraphAndFillWithMEASData() {
+//        final var g = createGraph();
+//        MEASData.fillGraphWithMEASData(g, numberOfAnalogValues, numberOfDiscreteValues);
+//        return g;
+//    }
+
+    @Benchmark
+    public List<QuerySolution> queryAnalogAndDigitalValuesWithPreparedQueries() {
+        return queryAnalogAndDigitalValuesWithPreparedQueries(sutDatasetGraph);
+    }
+
+    @Benchmark
+    public List<QuerySolution> queryAnalogAndDigitalValues() {
+        return queryAnalogAndDigitalValues(sutGraph);
+    }
+
+    @Benchmark
+    public Pair<List<AnalogValue>, List<DiscreteValue>> fillQueryResultsIntoLists() {
+        return fillQueryResultsIntoLists(sutQuerySolutions);
+    }
+
+    @Benchmark
+    public Pair<List<AnalogValue>, List<DiscreteValue>> fillListsByGraph() {
+        return fillListsByGraph(sutGraph);
+    }
+
+    private Pair<List<AnalogValue>, List<DiscreteValue>> fillListsByGraph(Graph g) {
+        final var analogValues = new ArrayList<AnalogValue>(numberOfAnalogValues);
+        final var discreteValues = new ArrayList<DiscreteValue>(numberOfDiscreteValues);
+        g.stream(Node.ANY, MEASData.AnalogValueValue.asNode(), Node.ANY)
                 .forEach(triple -> {
                     final var s = triple.getSubject();
-                    final var timeStamp = ((XSDDateTime) g.find(s, MeasurementValueTimeStamp.asNode(), Node.ANY).next().getObject().getLiteralValue()).asCalendar().toInstant();
-                    final var status = (int) g.find(s, MeasurementValueStatus.asNode(), Node.ANY).next().getObject().getLiteralValue();
+                    final var timeStamp = ((XSDDateTime) g.find(s, MEASData.MeasurementValueTimeStamp.asNode(), Node.ANY).next().getObject().getLiteralValue()).asCalendar().toInstant();
+                    final var status = (int) g.find(s, MEASData.MeasurementValueStatus.asNode(), Node.ANY).next().getObject().getLiteralValue();
                     analogValues.add(new AnalogValue((float) triple.getObject().getLiteralValue(), timeStamp, status));
                 });
-        g.stream(Node.ANY, DiscreteValueValue.asNode(), Node.ANY)
+        g.stream(Node.ANY, MEASData.DiscreteValueValue.asNode(), Node.ANY)
                 .forEach(triple -> {
                     final var s = triple.getSubject();
-                    final var timeStamp = ((XSDDateTime) g.find(s, MeasurementValueTimeStamp.asNode(), Node.ANY).next().getObject().getLiteralValue()).asCalendar().toInstant();
-                    final var status = (int) g.find(s, MeasurementValueStatus.asNode(), Node.ANY).next().getObject().getLiteralValue();
+                    final var timeStamp = ((XSDDateTime) g.find(s, MEASData.MeasurementValueTimeStamp.asNode(), Node.ANY).next().getObject().getLiteralValue()).asCalendar().toInstant();
+                    final var status = (int) g.find(s, MEASData.MeasurementValueStatus.asNode(), Node.ANY).next().getObject().getLiteralValue();
                     discreteValues.add(new DiscreteValue((int) triple.getObject().getLiteralValue(), timeStamp, status));
                 });
-        stopWatch.stop();
-        System.out.println("Iterating over graph took " + stopWatch.getTime() + "ms and returned " + (analogValues.size() + discreteValues.size()) + " results.");
+        return Pair.of(analogValues, discreteValues);
     }
 
-    private static class NextStringInArrayProvider {
-        private final String[] array;
-        private int index = 0;
 
-        public NextStringInArrayProvider(String[] array) {
-            this.array = array;
-        }
-
-        public String next() {
-            if (index == array.length) {
-                index = 0;
-            }
-            return array[index++];
-        }
-    }
 
     private record AnalogValue(float value, Instant timeStamp, int status) {
     }
 
     private record DiscreteValue(int value, Instant timeStamp, int status) {
+    }
+
+    @Setup(Level.Trial)
+    public void setupTrial() throws Exception {
+        this.numberOfAnalogValues = Integer.parseInt(param1_numberOfAnalogValues);
+        this.numberOfDiscreteValues = Integer.parseInt(param2_numberOfDiscreteValues);
+        this.sutGraph = createGraph();
+        MEASData.fillGraphWithMEASData(this.sutGraph, numberOfAnalogValues, numberOfDiscreteValues);
+        this.sutDatasetGraph = DatasetGraphFactory.wrap(this.sutGraph);
+        this.sutAnalogQuery = QueryFactory.create("""
+                PREFIX meas: <http://www.fancyTSO.org/OurCIMModel/MEASv1#>
+                PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                SELECT ?s ?analogValue ?timeStamp ?status  
+                WHERE { ?s meas:AnalogValue.value ?analogValue;
+                           meas:MeasurementValue.timeStamp ?timeStamp;
+                           meas:MeasurementValue.status ?status.}
+                """);
+        this.sutDiscreteQuery = QueryFactory.create("""
+                PREFIX meas: <http://www.fancyTSO.org/OurCIMModel/MEASv1#>
+                PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                SELECT ?s ?discreteValue ?timeStamp ?status  
+                WHERE { ?s meas:DiscreteValue.value ?discreteValue;
+                           meas:MeasurementValue.timeStamp ?timeStamp;
+                           meas:MeasurementValue.status ?status.}
+                """);
+        this.sutQuerySolutions = queryAnalogAndDigitalValuesWithPreparedQueries(this.sutDatasetGraph);
+    }
+
+    @Test
+    public void benchmark() throws Exception {
+        var opt = JMHDefaultOptions.getDefaults(this.getClass())
+                .measurementIterations(15)
+                .build();
+        var results = new Runner(opt).run();
+        Assert.assertNotNull(results);
     }
 
 }
