6878b6bd773e258b0db52a9b43df6cf63bad9d3d
arne-bdt <arne.bernhardt@gmail.coom>
2025-06-20 15:13:58 +0200

    Unsorted and unfinished
    - got rid of consumer pattern at some points
    --> performance is only up to jena parser
    
    - TODO:
     - use only one single byte buffer per element (tag or text)
     - get rid of consumer pattern, as it is slow
     - Threading?

--- jena-benchmarks/jena-benchmarks-jmh/src/test/java/org/apache/jena/cimxml/utils/DecodingTextByteBuffer.java
+++ jena-benchmarks/jena-benchmarks-jmh/src/test/java/org/apache/jena/cimxml/utils/DecodingTextByteBuffer.java
@@ -18,101 +18,97 @@ public class DecodingTextByteBuffer extends StreamBufferChild {
         this.lastAmpersandPosition = -1;
     }
 
-
     @Override
-    public void consumeBytes(Consumer<Byte> byteConsumer) throws IOException {
-        var c = byteConsumer.andThen((b) -> {
-            switch (b) {
-                case AMPERSAND -> lastAmpersandPosition = position; // Store the position of the last '&'
-                case SEMICOLON -> {
-                    var charsBetweenAmpersandAndSemicolon = position - lastAmpersandPosition - 1;
-                    switch (charsBetweenAmpersandAndSemicolon) {
-                        case 2: {
-                            if (buffer[lastAmpersandPosition + 2] == 't') {
-                                if (buffer[lastAmpersandPosition + 1] == 'l') {
-                                    buffer[lastAmpersandPosition] = LEFT_ANGLE_BRACKET; // &lt;
-
-                                    // move remaining data to the left
-                                    System.arraycopy(buffer, position + 1,
-                                            buffer, lastAmpersandPosition + 1,
-                                            filledToExclusive - position);
-
-                                    filledToExclusive -= 3; // Reduce filledToExclusive by 3 for &lt;
-                                    position = lastAmpersandPosition;
-                                    lastAmpersandPosition = -1; // Reset last ampersand position
-                                    return;
-                                } else if (buffer[lastAmpersandPosition + 1] == 'g') {
-                                    buffer[lastAmpersandPosition] = RIGHT_ANGLE_BRACKET; // &gt;
-
-                                    // move remaining data to the left
-                                    System.arraycopy(buffer, position + 1,
-                                            buffer, lastAmpersandPosition + 1,
-                                            filledToExclusive - position);
-
-                                    filledToExclusive -= 3; // Reduce filledToExclusive by 3 for &gt;
-                                    position = lastAmpersandPosition;
-                                    lastAmpersandPosition = -1; // Reset last ampersand position
-                                    return;
-                                }
-                            }
-                            break;
-                        }
-                        case 3: {
-                            if (buffer[lastAmpersandPosition + 3] == 'p'
-                                    && buffer[lastAmpersandPosition + 2] == 'm'
-                                    && buffer[lastAmpersandPosition + 1] == 'a') {
-                                buffer[lastAmpersandPosition] = AMPERSAND; // &amp;
+    protected void afterConsumeCurrent() {
+        switch (buffer[position]) {
+            case AMPERSAND -> lastAmpersandPosition = position; // Store the position of the last '&'
+            case SEMICOLON -> {
+                var charsBetweenAmpersandAndSemicolon = position - lastAmpersandPosition - 1;
+                switch (charsBetweenAmpersandAndSemicolon) {
+                    case 2: {
+                        if (buffer[lastAmpersandPosition + 2] == 't') {
+                            if (buffer[lastAmpersandPosition + 1] == 'l') {
+                                buffer[lastAmpersandPosition] = LEFT_ANGLE_BRACKET; // &lt;
 
                                 // move remaining data to the left
                                 System.arraycopy(buffer, position + 1,
                                         buffer, lastAmpersandPosition + 1,
                                         filledToExclusive - position);
 
-                                filledToExclusive -= 4; // Reduce filledToExclusive by 4 for &amp;
+                                filledToExclusive -= 3; // Reduce filledToExclusive by 3 for &lt;
+                                position = lastAmpersandPosition;
+                                lastAmpersandPosition = -1; // Reset last ampersand position
+                                return;
+                            } else if (buffer[lastAmpersandPosition + 1] == 'g') {
+                                buffer[lastAmpersandPosition] = RIGHT_ANGLE_BRACKET; // &gt;
+
+                                // move remaining data to the left
+                                System.arraycopy(buffer, position + 1,
+                                        buffer, lastAmpersandPosition + 1,
+                                        filledToExclusive - position);
+
+                                filledToExclusive -= 3; // Reduce filledToExclusive by 3 for &gt;
                                 position = lastAmpersandPosition;
                                 lastAmpersandPosition = -1; // Reset last ampersand position
                                 return;
                             }
-                            break;
                         }
-                        case 4: {
-                            if (buffer[lastAmpersandPosition + 3] == 'o') {
-                                if (buffer[lastAmpersandPosition + 1] == 'q'
-                                        && buffer[lastAmpersandPosition + 2] == 'u'
-                                        && buffer[lastAmpersandPosition + 4] == 't') {
-                                    buffer[lastAmpersandPosition] = DOUBLE_QUOTE; // &quot;
+                        break;
+                    }
+                    case 3: {
+                        if (buffer[lastAmpersandPosition + 3] == 'p'
+                                && buffer[lastAmpersandPosition + 2] == 'm'
+                                && buffer[lastAmpersandPosition + 1] == 'a') {
+                            buffer[lastAmpersandPosition] = AMPERSAND; // &amp;
 
-                                    // move remaining data to the left
-                                    System.arraycopy(buffer, position + 1,
-                                            buffer, lastAmpersandPosition + 1,
-                                            filledToExclusive - position);
+                            // move remaining data to the left
+                            System.arraycopy(buffer, position + 1,
+                                    buffer, lastAmpersandPosition + 1,
+                                    filledToExclusive - position);
 
-                                    filledToExclusive -= 5; // Reduce filledToExclusive by 5 for &quot;
-                                    position = lastAmpersandPosition;
-                                    lastAmpersandPosition = -1; // Reset last ampersand position
-                                    return;
-                                } else if (buffer[lastAmpersandPosition + 2] == 'p'
-                                        && buffer[lastAmpersandPosition + 4] == 's'
-                                        && buffer[lastAmpersandPosition + 1] == 'a') {
-                                    buffer[lastAmpersandPosition] = SINGLE_QUOTE; // &apos;
+                            filledToExclusive -= 4; // Reduce filledToExclusive by 4 for &amp;
+                            position = lastAmpersandPosition;
+                            lastAmpersandPosition = -1; // Reset last ampersand position
+                            return;
+                        }
+                        break;
+                    }
+                    case 4: {
+                        if (buffer[lastAmpersandPosition + 3] == 'o') {
+                            if (buffer[lastAmpersandPosition + 1] == 'q'
+                                    && buffer[lastAmpersandPosition + 2] == 'u'
+                                    && buffer[lastAmpersandPosition + 4] == 't') {
+                                buffer[lastAmpersandPosition] = DOUBLE_QUOTE; // &quot;
 
-                                    // move remaining data to the left
-                                    System.arraycopy(buffer, position + 1,
-                                            buffer, lastAmpersandPosition + 1,
-                                            filledToExclusive - position);
+                                // move remaining data to the left
+                                System.arraycopy(buffer, position + 1,
+                                        buffer, lastAmpersandPosition + 1,
+                                        filledToExclusive - position);
 
-                                    filledToExclusive -= 5; // Reduce filledToExclusive by 5 for &apos;
-                                    position = lastAmpersandPosition;
-                                    lastAmpersandPosition = -1; // Reset last ampersand position
-                                    return;
-                                }
+                                filledToExclusive -= 5; // Reduce filledToExclusive by 5 for &quot;
+                                position = lastAmpersandPosition;
+                                lastAmpersandPosition = -1; // Reset last ampersand position
+                                return;
+                            } else if (buffer[lastAmpersandPosition + 2] == 'p'
+                                    && buffer[lastAmpersandPosition + 4] == 's'
+                                    && buffer[lastAmpersandPosition + 1] == 'a') {
+                                buffer[lastAmpersandPosition] = SINGLE_QUOTE; // &apos;
+
+                                // move remaining data to the left
+                                System.arraycopy(buffer, position + 1,
+                                        buffer, lastAmpersandPosition + 1,
+                                        filledToExclusive - position);
+
+                                filledToExclusive -= 5; // Reduce filledToExclusive by 5 for &apos;
+                                position = lastAmpersandPosition;
+                                lastAmpersandPosition = -1; // Reset last ampersand position
+                                return;
                             }
-                            break;
                         }
+                        break;
                     }
                 }
             }
-        });
-        super.consumeBytes(c);
+        }
     }
 }
